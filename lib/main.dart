1a53e4e515d2ae37cb95b4c88452314973bfdc73
AzizAhsaan <azizahsaan339@gmail.com>
2024-07-31 09:57:43 +0300

    manually triggering the theme provider with a button and onTap function

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:simple_toggle_theme/pages/home_page.dart';
 import 'package:simple_toggle_theme/theme/theme.dart';
+import 'package:simple_toggle_theme/theme/theme_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -14,8 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-      theme: lightMode,
-      darkTheme: darkMode,
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
